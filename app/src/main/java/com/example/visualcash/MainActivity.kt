c86c8874d35a5152758daf3a04690f7c85d0cc2e
Ricardo Soto <ricardosr4@gmail.com>
2026-02-23 20:48:11 -0300

    primer comit

---
+++ app/src/main/java/com/example/visualcash/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.visualcash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.visualcash.ui.theme.VisualCashTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VisualCashTheme {
+
+            }
+        }
+    }
+}
